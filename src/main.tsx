479b43e510f6647b7c4c9c2a962c876763d21448
gitCane <kumarsetu.work@gmail.com>
2025-10-13 21:41:29 +0530

    barood ka gola

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import emailjs from '@emailjs/browser';
 import App from './App';
 import './index.css';
+
+// Initialize EmailJS with your public key
+emailjs.init('lLH6--0YXEhBlso6i');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
